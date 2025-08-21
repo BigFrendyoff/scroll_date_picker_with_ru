f77c803f3e317ea56c690ed974e3dfb6c35f080a
BigFrendyoff <91464222+BigFrendyoff@users.noreply.github.com>
2025-08-21 14:53:45 +0300

    fix

--- lib/src/constants.dart
+++ lib/src/constants.dart
@@ -15,7 +15,7 @@ const String ar = 'ar';
 const String pt = 'pt';
 const String tr = 'tr';
 const String pl = 'pl';
-const String ru = ''ru';
+const String ru = 'ru';
 
 extension LocaleExtension on Locale {
   List<String> get months {
